58db0b4cde84c75f01b83c35b68b97244073ce8a
Vlad <vladyslav.balushka@gmail.com>
2025-11-06 23:58:57 +0200

    hot fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,13 +6,13 @@ import { QueryClientProvider } from "@tanstack/react-query"
 import { KeyboardProvider } from "react-native-keyboard-controller"
 import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
 
+import { PersistNavigation } from "@/config/config.base"
 import { ErrorBoundary } from "@/shared/components/ErrorBoundary/ErrorBoundary"
 import { initI18n } from "@/shared/i18n"
 import { queryClient } from "@/shared/services/api/query"
 import { ThemeProvider } from "@/shared/theme/context"
 import { customFontsToLoad } from "@/shared/theme/typography"
 import { loadDateFnsLocale } from "@/shared/utils/formatDate"
-import { PersistNavigation } from "@/config/config.base"
 
 SplashScreen.preventAutoHideAsync()
 
